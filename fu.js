0a0cdc509a10af0ec81a584b026ed947cc4eb01d
BuildTools <unconfigured@null.spigotmc.org>
2021-09-01 16:21:36 +0200

    weq

---
+++ fu.js
@@ -0,0 +1,29 @@
+class pocitan{
+    constructor(x,y){
+        this.x = x;
+        this.y = y; 
+    }
+    nasobeni(d){
+        d = this.x*this.y
+        return d;
+    };
+    deleni(){
+        console.log(this.x/this.y);
+    };
+    plus(){
+        console.log(this.x+this.y);
+    };
+    minus(){
+        console.log(this.x-this.y);
+    };
+    plusominusoadelenonasobeni(){
+        console.log(this.x + this.y + this.x - this.y + this.x / this.y + this.x * this.y);
+    };
+};
+let d = 0;
+const pocN = new pocitan(4,5);
+pocN.nasobeni(console.log(d));
+pocN.deleni();
+pocN.plus();
+pocN.minus();
+pocN.plusominusoadelenonasobeni();
