590e4d8d5b0aaca452b0162bb9e02b51ba022d73
Miroo <mirdajakes@gmail.com>
2021-09-01 21:51:39 +0200

    sdsd

--- fu.js
+++ fu.js
@@ -1,4 +1,4 @@
-class pocitan{
+/*class pocitan{
     constructor(x,y){
         this.x = x;
         this.y = y; 
@@ -26,5 +26,4 @@ pocN.nasobeni(console.log(d));
 pocN.deleni();
 pocN.plus();
 pocN.minus();
-pocN.plusominusoadelenonasobeni();
-/* JUST SPAM DONT USE IT */
+pocN.plusominusoadelenonasobeni();*/
